a22ea94163cb124e72a3a0273ebf5c6450fe333d
AlexKatrenich <alex.katrenich@gmail.com>
2019-12-12 14:30:19 +0200

    Lock screen orientation

--- KatrenichTestApp/app/src/main/java/com/katrenich/testapp/data/di/ApiModule.kt
+++ KatrenichTestApp/app/src/main/java/com/katrenich/testapp/data/di/ApiModule.kt
@@ -10,6 +10,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
@@ -24,6 +25,11 @@ class ApiModule {
 	fun providesHttpOkClient(): OkHttpClient =
 		OkHttpClient
 			.Builder()
+			.apply {
+				connectTimeout(30, TimeUnit.SECONDS)
+				readTimeout(30, TimeUnit.SECONDS)
+				writeTimeout(30, TimeUnit.SECONDS)
+			}
 			.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
 			.build()
 
